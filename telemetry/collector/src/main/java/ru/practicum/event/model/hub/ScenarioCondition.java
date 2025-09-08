b6d93da26ead6d464508445611e764c90cf83b03
Ivan G <grikoivan@yandex.ru>
2025-09-09 01:54:54 +0300

    Проблемы с импортом схем Авро

---
+++ telemetry/collector/src/main/java/ru/practicum/event/model/hub/ScenarioCondition.java
@@ -0,0 +1,28 @@
+package ru.practicum.event.model.hub;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ScenarioCondition {
+
+    @NotBlank
+    String sensorId;
+
+    @NotNull
+    ConditionType conditionType;
+
+    @NotNull
+    ConditionOperation conditionOperation;
+
+    @NotNull
+    Integer value;
+}
