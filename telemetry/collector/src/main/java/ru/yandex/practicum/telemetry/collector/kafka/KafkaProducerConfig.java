eea8a74c0cf62a843f82721c0482ab0873bf2feb
Ivan G <grikoivan@yandex.ru>
2025-09-20 19:00:56 +0300

    2-collector-grpc

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/KafkaProducerConfig.java
@@ -35,6 +35,7 @@ public class KafkaProducerConfig {
                 config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
                 config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
                 config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralKafkaSerializer.class);
+
                 producer = new KafkaProducer<>(config);
             }
 
