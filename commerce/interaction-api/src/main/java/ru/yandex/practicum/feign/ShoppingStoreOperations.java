fbba9497cbe850db0179dc245a9d8d75ef114b0f
Ivan G <grikoivan@yandex.ru>
2025-10-17 21:35:55 +0300

    spring-boot-maven-plugin added

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/ShoppingStoreOperations.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/ShoppingStoreOperations.java
@@ -2,13 +2,8 @@ package ru.yandex.practicum.feign;
 
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.dto.Pageable;
-import ru.yandex.practicum.dto.ProductCategory;
 import ru.yandex.practicum.dto.ProductDto;
 import ru.yandex.practicum.dto.SetProductQuantityStateRequest;
 
@@ -21,8 +16,14 @@ public interface ShoppingStoreOperations {
     @PutMapping
     ProductDto addProduct(@Valid @RequestBody ProductDto product);
 
-    @GetMapping
-    Collection<ProductDto> searchProducts(@PathVariable String category, @PathVariable Pageable params);
+//    @GetMapping
+//    Collection<ProductDto> searchProducts(@PathVariable String category, @PathVariable Pageable params);
+
+    @GetMapping("/products")
+    Collection<ProductDto> searchProducts(
+            @RequestParam(value = "category", required = false) String category,
+            Pageable pageable
+    );
 
     @GetMapping("/{productId}")
     ProductDto getProductById(@PathVariable UUID productId);
