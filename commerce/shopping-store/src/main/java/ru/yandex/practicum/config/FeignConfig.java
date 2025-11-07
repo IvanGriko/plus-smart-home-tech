9e3a114335b2afdb1bd55e30045a0f3aca504977
Ivan G <grikoivan@yandex.ru>
2025-11-08 01:46:20 +0300

    class FeignConfig

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/config/FeignConfig.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class FeignConfig {}
